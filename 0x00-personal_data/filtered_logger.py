8e714e42d29a0dbc880bc72290b8c95698e8fa26
Outtacosmos-ai <messghir4@gmail.com>
2024-11-07 22:00:13 +0100

    done ✅

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,13 +7,21 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(
+    fields: List[str],
+    redaction: str,
+    message: str,
+    separator: str
+) -> str:
     """
     Returns the log message obfuscated
     """
     for field in fields:
-        message = re.sub(f'{field}=.*?{separator}',
-                         f'{field}={redaction}{separator}', message)
+        message = re.sub(
+            f'{field}=.*?{separator}',
+            f'{field}={redaction}{separator}',
+            message
+        )
     return message
 
 
@@ -21,8 +29,10 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
 if __name__ == "__main__":
     fields = ["password", "date_of_birth"]
     messages = [
-        "name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;",
-        "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"
+        "name=egg;email=eggmin@eggsample.com;password=eggcellent;"
+        "date_of_birth=12/12/1986;",
+        "name=bob;email=bob@dylan.com;password=bobbycool;"
+        "date_of_birth=03/04/1993;"
     ]
 
     for message in messages:
