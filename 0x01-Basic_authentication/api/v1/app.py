60ad5692c3d1e44155da1d1ea67b6851c9fee13b
Outtacosmos-ai <messghir4@gmail.com>
2024-11-14 21:20:09 +0100

    XAVI

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -5,33 +5,27 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
+from flask_cors import CORS
 
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = None
 
 auth_type = getenv("AUTH_TYPE")
-
-if auth_type and auth_type == "auth":
-    from api.v1.auth.auth import Auth
-    auth = Auth()
-if auth_type and auth_type == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
+auth = BasicAuth() if auth_type == "basic_auth" else Auth()
 
 
 @app.before_request
 def before_request() -> None:
-    """ Before request
-    """
+    """ Before request """
     if auth is None:
         return
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+    excluded_paths = [
+        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'
+    ]
     if not auth.require_auth(request.path, excluded_paths):
         return
     if not auth.authorization_header(request):
@@ -42,22 +36,19 @@ def before_request() -> None:
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """ Not found handler """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized_error(error) -> str:
-    """ Unauthorized handler
-    """
+    """ Unauthorized handler """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden_error(error) -> str:
-    """ Forbidden handler
-    """
+    """ Forbidden handler """
     return jsonify({"error": "Forbidden"}), 403
 
 
