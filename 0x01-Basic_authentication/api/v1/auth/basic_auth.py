810091d547bd33fe4c9a15d80c905af339bcfeb1
Outtacosmos-ai <messghir4@gmail.com>
2024-11-14 20:54:12 +0100

    all at once

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,66 +1,84 @@
 #!/usr/bin/env python3
+""" basic_auth.py
 """
-Basic Auth module
-"""
-
 from api.v1.auth.auth import Auth
-from typing import TypeVar, List
-from models.user import User
 import base64
-import binascii
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
+    """ BasicAuth class
     """
-    class BasicAuth
-    """
-
     def extract_base64_authorization_header(
-            self, authorization_header: str) -> str:
+        self, authorization_header: str
+    ) -> str:
+        """ Method that should implement the logic for checking if a request
         """
-         returns the Base64 part of the Authorization
-         header for a Basic Authentication
-        """
-        if (authorization_header is None or
-                not isinstance(authorization_header, str) or
-                not authorization_header.startswith("Basic")):
+        if authorization_header is None:
             return None
-
-        return authorization_header[6:]
+        if type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        else:
+            return authorization_header[6:]
 
     def decode_base64_authorization_header(
-            self, base64_authorization_header: str) -> str:
+        self, base64_authorization_header: str
+    ) -> str:
+        """ Method that should implement the logic for checking if a request
         """
-        returns the decoded value of a Base64
-        string base64_authorization_header
-        """
-        b64_auth_header = base64_authorization_header
-        if b64_auth_header and isinstance(b64_auth_header, str):
-            try:
-                encode = b64_auth_header.encode('utf-8')
-                base = base64.b64decode(encode)
-                return base.decode('utf-8')
-            except binascii.Error:
-                return None
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            header = base64_authorization_header.encode('utf-8')
+            header = base64.b64decode(header)
+            return header.decode('utf-8')
+        except Exception:
+            return None
 
     def extract_user_credentials(
-            self, decoded_base64_authorization_header: str) -> (str, str):
+        self, decoded_base64_authorization_header: str
+    ) -> (str, str):
+        """ Method that should implement the logic for checking if a request
         """
-        returns the user email and password from the Base64 decoded value.
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if type(decoded_base64_authorization_header) is not str:
+            return (None, None)
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        user, password = decoded_base64_authorization_header.split(':', 1)
+        return (user, password)
+
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+    ) -> TypeVar('User'):
+        """ Method that should implement the logic for checking if a request
         """
-        decoded_64 = decoded_base64_authorization_header
-        if (decoded_64 and isinstance(decoded_64, str) and
-                ":" in decoded_64):
-            res = decoded_64.split(":", 1)
-            return (res[0], res[1])
-        return (None, None)
+        if user_email is None or user_pwd is None:
+            return None
+        if type(user_email) is not str or type(user_pwd) is not str:
+            return None
+        try:
+            users = User.search({'email': user_email})
+            if users is None or users == []:
+                return None
+            for user in users:
+                if not user.is_valid_password(user_pwd):
+                    return None
+                return user
+        except Exception:
+            return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """
-        overloads Auth and retrieves the User instance for a request
+        """ Method that should implement the logic for checking if a request
         """
         header = self.authorization_header(request)
-        b64header = self.extract_base64_authorization_header(header)
-        decoded = self.decode_base64_authorization_header(b64header)
-        user_creds = self.extract_user_credentials(decoded)
-        return self.user_object_from_credentials(*user_creds)
+        base64_header = self.extract_base64_authorization_header(header)
+        decoded_header = self.decode_base64_authorization_header(base64_header)
+        user, pwd = self.extract_user_credentials(decoded_header)
+        return self.user_object_from_credentials(user, pwd)
