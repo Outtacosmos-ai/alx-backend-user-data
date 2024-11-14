abd28c551f443d8d1ba73566995c22e928af6ad2
Outtacosmos-ai <messghir4@gmail.com>
2024-11-14 19:23:56 +0100

    test the changes

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -8,41 +8,49 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+
 auth_type = getenv("AUTH_TYPE")
 
-if auth_type == "auth":
+if auth_type and auth_type == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif auth_type == "basic_auth":
+if auth_type and auth_type == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+if auth_type and auth_type == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+if auth_type and auth_type == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+if auth_type and auth_type == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
 @app.before_request
-def before_request():
-    """
-    Filter each request before it's handled by the proper route
+def before_request() -> None:
+    """ Before request
     """
     if auth is None:
         return
-
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
-
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
+    auth_header = auth.authorization_header(request)
+    session_cookie = auth.session_cookie(request)
 
-    if auth.authorization_header(request) is None:
+    if not auth_header and not session_cookie:
         abort(401)
-
-    request.current_user = auth.current_user(request)
-    if request.current_user is None:
+    if not auth.current_user(request):
         abort(403)
+    request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
@@ -53,14 +61,14 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
+def unauthorized_error(error) -> str:
     """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error) -> str:
+def forbidden_error(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
