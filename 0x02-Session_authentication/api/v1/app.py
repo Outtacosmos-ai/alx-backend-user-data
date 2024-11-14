1aee70e0ecb1fb4efec09832bf5d3a53ef21207a
Outtacosmos-ai <messghir4@gmail.com>
2024-11-14 19:38:32 +0100

    Et moi et moi et moi!

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -13,44 +13,33 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+auth_type = getenv('AUTH_TYPE')
 
-auth_type = getenv("AUTH_TYPE")
-
-if auth_type and auth_type == "auth":
-    from api.v1.auth.auth import Auth
-    auth = Auth()
-if auth_type and auth_type == "basic_auth":
+if auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-if auth_type and auth_type == "session_auth":
-    from api.v1.auth.session_auth import SessionAuth
-    auth = SessionAuth()
-if auth_type and auth_type == "session_exp_auth":
-    from api.v1.auth.session_exp_auth import SessionExpAuth
-    auth = SessionExpAuth()
-if auth_type and auth_type == "session_db_auth":
-    from api.v1.auth.session_db_auth import SessionDBAuth
-    auth = SessionDBAuth()
-
 
 @app.before_request
-def before_request() -> None:
-    """ Before request
+def before_request():
+    """
+    Filter each request before it's handled by the proper route
     """
     if auth is None:
         return
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
+
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+
     if not auth.require_auth(request.path, excluded_paths):
         return
-    auth_header = auth.authorization_header(request)
-    session_cookie = auth.session_cookie(request)
 
-    if not auth_header and not session_cookie:
+    if auth.authorization_header(request) is None:
         abort(401)
-    if not auth.current_user(request):
-        abort(403)
+
     request.current_user = auth.current_user(request)
+    if request.current_user is None:
+        abort(403)
 
 
 @app.errorhandler(404)
@@ -61,14 +50,14 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized_error(error) -> str:
+def unauthorized(error) -> str:
     """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden_error(error) -> str:
+def forbidden(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
