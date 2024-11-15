40f284c2f4999b90a203f89c7377df6c6858f3bc
Outtacosmos-ai <messghir4@gmail.com>
2024-11-15 11:56:29 +0100

    Black lake

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,34 +1,80 @@
 #!/usr/bin/env python3
-
-from flask import Flask, jsonify, request, abort 
-
-from models import storage
-from api.v1.views import app_views
+"""
+Route module for the API
+"""
 from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+auth_type = getenv("AUTH_TYPE")
+
+if auth_type and auth_type == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+if auth_type and auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+if auth_type and auth_type == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+if auth_type and auth_type == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+if auth_type and auth_type == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
 @app.before_request
-def before_request():
-    """ Example function """
-    pass
+def before_request() -> None:
+    """ Before request
+    """
+    if auth is None:
+        return
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    auth_header = auth.authorization_header(request)
+    session_cookie = auth.session_cookie(request)
+
+    if not auth_header and not session_cookie:
+        abort(401)
+    if not auth.current_user(request):
+        abort(403)
+    request.current_user = auth.current_user(request)
 
 
-@app.teardown_appcontext
-def teardown_appcontext(exception):
-    """ Remove current SQLAlchemy session """
-    storage.close()
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
 
 
-@app.route('/status', methods=['GET'])
-def status():
-    """Returns the status of the API"""
-    return jsonify({"status": "OK"})
+@app.errorhandler(401)
+def unauthorized_error(error) -> str:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
 
 
-@app.route('/stats', methods=['GET'])
-def stats():
-    """Returns the number of each object type"""
-    return jsonify(storage.count())
+@app.errorhandler(403)
+def forbidden_error(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
